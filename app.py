c852a0ade65031119d9e09cb70fb83abc5539df0
arturpedrotti <arturgrochau@gmail.com>
2024-12-03 00:31:52 -0300

    Ensure Flask integration for graph handling and improve app accessibility

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 from flask import Flask, render_template, redirect, url_for
 import subprocess
+import socket
 
 app = Flask(__name__)
+
+# Get local IP address
+def get_local_ip():
+    return socket.gethostbyname(socket.gethostname())
 
 # Landing page route
 @app.route('/')
@@ -13,14 +18,14 @@ def landing_page():
 def data_insights():
     # Start the Data Insights app if needed
     subprocess.Popen(['python', 'data_insights/app.py'], close_fds=True)
-    return redirect("http://127.0.0.1:8050")
+    return redirect(f"http://{get_local_ip()}:8050")
 
 # Redirect to Graph Builder sub-app
 @app.route('/graph_builder/')
 def graph_builder():
     # Start the Graph Builder app if needed
     subprocess.Popen(['python', 'graph_builder/app.py'], close_fds=True)
-    return redirect("http://127.0.0.1:8060")
+    return redirect(f"http://{get_local_ip()}:8060")
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
